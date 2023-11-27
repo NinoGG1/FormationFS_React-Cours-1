80ef127d9ba3fbb98d1e8a1be3a9f6d101f2c3aa
Nino GUEGUEN <ninogueguen@gmail.com>
2023-11-27 12:18:19 +0100

    Premier Commit

---
+++ src/pages/About.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        amet quia aperiam laboriosam enim minima, harum asperiores esse vero
+        suscipit, iure corporis eaque ad ipsum. Dolor porro adipisci sunt
+        molestias eum velit earum quae ipsam inventore vitae! Expedita eaque,
+        nisi esse ut ex aliquam cupiditate, consectetur suscipit unde quibusdam
+        eius perferendis modi? Rem corporis veniam laudantium, consequuntur est
+        sed culpa. Praesentium quis eos id! Cum, cupiditate fuga. Repellat et
+        sint, similique, dolorum sapiente quis mollitia consequuntur recusandae
+        neque exercitationem blanditiis molestiae corrupti quod dignissimos
+        maiores esse! Obcaecati error tempora sapiente possimus ipsam magnam vel
+        aliquam repudiandae similique, eos unde nesciunt, nisi dolorem a.
+        Distinctio perferendis aliquam commodi. Quod, consequuntur minima
+        aliquid at, totam expedita et hic odit enim, suscipit inventore debitis
+        cupiditate voluptatibus corrupti. Nihil, ducimus. Voluptate eveniet
+        ipsam iusto ut consequuntur corrupti quasi animi rem veritatis! Expedita
+        cum at, illum reprehenderit neque nisi, voluptatibus aliquam nesciunt
+        mollitia sapiente praesentium commodi voluptatum illo nihil deleniti in
+        sint voluptatem quae laboriosam hic possimus voluptates soluta?
+        Voluptate perspiciatis unde incidunt eligendi eius. Ipsum veritatis
+        assumenda natus quod nostrum voluptas fugiat itaque, dolorem dolor
+        ducimus? Exercitationem sit enim dolore neque quis, nesciunt iure totam
+        quod! Corporis, magni minus accusamus possimus culpa dicta laborum?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        debitis recusandae voluptate? Non incidunt quasi rerum numquam itaque.
+        Perferendis, officiis necessitatibus ex veniam tenetur dolores beatae
+        fuga quasi ducimus sed. Enim ab debitis pariatur quasi ullam, molestias
+        repellendus voluptatum atque et nisi quas nam placeat vitae. Unde nihil
+        fugiat odio. Atque dicta fugiat accusamus, iste eligendi rem
+        reprehenderit expedita cumque aut, sint earum, animi facilis! Pariatur
+        excepturi blanditiis non voluptatibus. Magnam tempore quisquam minima,
+        architecto, quod officia est iste ea tenetur perspiciatis repellat
+        voluptas aut vel tempora asperiores ex! Ducimus voluptatem, ea similique
+        aliquam accusamus, praesentium sapiente ullam rerum perferendis,
+        asperiores velit perspiciatis. Aliquam similique minima eos fugiat
+        tenetur quas consectetur reiciendis adipisci. Maxime libero voluptatum
+        ea deserunt amet? Recusandae esse quidem mollitia! Tenetur eum animi
+        consequatur iste, itaque sed enim repellat voluptatum eius rerum
+        voluptates aliquam excepturi architecto soluta illo omnis, repellendus
+        ratione alias? Velit hic eaque autem atque incidunt, eos molestias
+        adipisci exercitationem deserunt fuga, labore aperiam vel animi
+        distinctio quis voluptates delectus perspiciatis beatae reprehenderit!
+        Earum, iste! Optio molestiae incidunt ratione consequuntur placeat
+        voluptatibus pariatur rem, eius et itaque exercitationem, aliquid
+        voluptas expedita, ex nostrum illo delectus. Autem, amet quibusdam
+        facilis doloribus velit unde tempore debitis voluptate!
+      </p>
+    </div>
+  );
+};
+
+export default About;
